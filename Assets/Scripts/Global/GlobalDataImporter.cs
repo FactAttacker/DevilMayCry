98f14d069370e1c869efde5bf4712dee09656e52
박도현 <pdh@ideasam.net>
2021-09-17 01:03:09 +0900

    구글 엑셀 데이터 Class형태로  파싱

---
+++ Assets/Scripts/Global/GlobalDataImporter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+public class GlobalDataImporter : MonoBehaviour
+{
+    [SerializeField]
+    GoogleSheetData googleSheetData;
+
+    [System.Serializable]
+    public class GoogleSheetData
+    {
+        [Header("플레이어")]
+        public TextAsset player;
+
+        [Header("스킬")]
+        public TextAsset skill;
+    }
+
+    void Awake()
+    {
+        //Player 정보 담기
+        GlobalState.playerList = JsonConvert.DeserializeObject<List<Excel_Player>>(googleSheetData.player.ToString());
+        
+        //Skill 정보 담기
+        GlobalState.skillList = JsonConvert.DeserializeObject<List<Excel_Skill>>(googleSheetData.skill.ToString());
+    }
+}

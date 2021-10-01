1d17e693e970d58cfcc63b9fdf015f2a2d5339ae
dozerlourd <54570921+dozerlourd@users.noreply.github.com>
2021-10-02 02:40:19 +0900

    Boss Hp Bar and Camera Shake

--- Assets/Scripts/Boss/BossHP.cs
+++ Assets/Scripts/Boss/BossHP.cs
@@ -5,12 +5,24 @@ using UnityEngine.UI;
 
 public class BossHP : MonoBehaviour
 {
-    Coroutine Co_damaged;
+    #region 변수
 
-    [SerializeField] Text text;
+    Coroutine Co_damaged;
+    Coroutine Co_RefreshHPBar;
+
+    // 배경 HP바 이미지, 중간 흰 막대 이미지(HP바 끝 이미지), HP바 이미지. Damage Hp바 이미지
+    [SerializeField] Image hpBar, damagedHPBar;
 
     [SerializeField, Range(0f, 1f), Tooltip("행 = HP 비율, 열 = 0(애니메이션 실행 불가능) 1(애니메이션 실행 가능 -> 세팅은 전부 1로)")] float[] damageRates;
-    [Range(0, 1), Tooltip("0(애니메이션 실행 불가능), 1(애니메이션 실행 가능)")] int[] damageAnimPosibleCounts;
+    [SerializeField, Range(0, 1), Tooltip("0(애니메이션 실행 불가능), 1(애니메이션 실행 가능)")] int[] damageAnimPosibleCounts;
+
+    [SerializeField, Tooltip("HP Bar UI의 fillAmount 변화 속도")] float refreshSpeed = 1;
+    [SerializeField, Tooltip("붉은 HP Bar 변화 전 대기시간")] float refreshWaitTime = 0.1f;
+    WaitForSeconds waitRefreshSeconds;
+
+    #endregion
+
+    #region 속성
 
     public float CurrHP
     {
@@ -23,6 +35,7 @@ public class BossHP : MonoBehaviour
                 if (CurrHP / MaxHP <= damageRates[i] && damageAnimPosibleCounts[i] == 1)
                 {
                     Co_damaged = StartCoroutine(Co_Damaged());
+                    damageAnimPosibleCounts[i] = 0;
                     break;
                 }
             }
@@ -37,6 +50,16 @@ public class BossHP : MonoBehaviour
         private set => maxHP = value;
     }
     [SerializeField] float maxHP = 100;
+    public float TakeDamage { set => CurrHP -= CurrHP - value <= 0 ? currHP : value; }
+
+    #endregion
+
+    #region 유니티 생명 주기
+
+    private void Start()
+    {
+        waitRefreshSeconds = new WaitForSeconds(refreshWaitTime);
+    }
 
     private void OnEnable()
     {
@@ -48,16 +71,31 @@ public class BossHP : MonoBehaviour
         CurrHP = MaxHP;
     }
 
+    #endregion
 
-    public float TakeDamage { set => CurrHP -= CurrHP - value <= 0 ? currHP : value; }
+    #region 구현부
+
     void RefreshHPBar(float value)
     {
-        text.text = value.ToString();
+        if (Co_RefreshHPBar != null) StopCoroutine(Co_RefreshHPBar); 
+        Co_RefreshHPBar = StartCoroutine(Co_RefreshHPBarCycle(value));
     }
 
-    public void Damage(float dmg)
+    #region 코루틴
+
+    IEnumerator Co_RefreshHPBarCycle(float _value)
     {
-        TakeDamage = dmg;
+        damagedHPBar.fillAmount = hpBar.fillAmount;
+        hpBar.fillAmount = _value / MaxHP;
+        yield return waitRefreshSeconds;
+
+        float rate = 0;
+        while (rate < 1)
+        {
+            rate += Time.deltaTime;
+            damagedHPBar.fillAmount = Mathf.Lerp(damagedHPBar.fillAmount, hpBar.fillAmount, rate * refreshSpeed);
+            yield return null;
+        }
     }
 
     public IEnumerator Co_Damaged()
@@ -74,4 +112,17 @@ public class BossHP : MonoBehaviour
 
         StopCoroutine(Co_damaged);
     }
+
+    #endregion
+
+    #endregion
+
+    #region 테스트용
+
+    public void Damage(float dmg)
+    {
+        TakeDamage = dmg;
+    }
+
+    #endregion
 }
--- Assets/Scripts/Boss/BossStateMachine.cs
+++ Assets/Scripts/Boss/BossStateMachine.cs
@@ -38,4 +38,13 @@ public class BossStateMachine : MonoBehaviour
         Debug.Log($"m_prevState : {prevState}");
         Debug.Log($"m_currentState : {currState}");
     }
+
+
+    /// <summary> 카메라 흔들림을 구현하는 OnShake 함수를 애니메이션으로 제어할 수 있도록 변환한 함수 </summary>
+    /// <param name="timeAndAmount"> time과 amount를 쉼표(,)를 이용하여 구분  </param>
+    public void OnCameraShake(string timeAndAmount)
+    {
+        string[] temps = timeAndAmount.Split(',');
+        CameraManager.instance.OnShake(float.Parse(temps[0]), float.Parse(temps[1]));
+    }
 }
